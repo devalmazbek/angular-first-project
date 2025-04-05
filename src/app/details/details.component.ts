63d7730794f166d8e7a4a6b8ac7ba22c3e1fdeb8
devalmazbek <devalmazbek@gmail.com>
2025-04-05 14:14:38 +0600

    implementation: detail page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <p>
-      details works!
+      details works! {{housingLocationId}}
     </p>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
+
 
 }
