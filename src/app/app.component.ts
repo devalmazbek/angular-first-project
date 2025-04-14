9573842cccf9db819d957a450d7c93fb43bd403d
devalmazbek <devalmazbek@gmail.com>
2025-04-14 12:14:08 +0600

    feat: details page, add housting service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,11 @@ import { RouterModule } from '@angular/router';
   selector: 'app-root',
   template: /*html*/`
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+       <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
